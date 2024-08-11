82bd2af282e8447828a3dfd4c84c566162093424
AfridMcCrea <73422624+AfridMcCrea@users.noreply.github.com>
2024-08-12 01:25:41 +0530

    page setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+
+
+const DasboardPage = () => {
+    return (
+        <div className="">
+            DashBoard Page
+        </div>
+    )
+};
+
+export default DasboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,5 @@
+const LandingPage = () => {
+  return <div className="">LandingPage(unprotected)</div>;
+};
+
+export default LandingPage;
